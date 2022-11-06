e6ad9b6787973476c3f307721913662db85da01f
Dragos0305 <stratulatdragos2000@gmail.com>
2022-11-06 06:01:43 -0800

    Add push and pop functions

--- Stack.java
+++ Stack.java
@@ -17,3 +17,43 @@ class Stack {
   }
 
 }
+
+// push elements to the top of stack
+  public void push(int x) {
+    if (isFull()) {
+      System.out.println("Stack OverFlow");
+
+      // terminates the program
+      System.exit(1);
+    }
+
+    // insert element on top of stack
+    System.out.println("Inserting " + x);
+    arr[++top] = x;
+  }
+
+  // check if the stack is full
+  public Boolean isFull() {
+    return top == capacity - 1;
+  }
+
+  // pop elements from top of stack
+  public int pop() {
+
+    // if stack is empty
+    // no element to pop
+    if (isEmpty()) {
+      System.out.println("STACK EMPTY");
+      // terminates the program
+      System.exit(1);
+    }
+
+    // pop element from top of stack
+    return arr[top--];
+  }
+
+   // check if the stack is empty
+  public Boolean isEmpty() {
+    return top == -1;
+  }
+}
