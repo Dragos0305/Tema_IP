370a4dd3a49302fe3fad9df07a05eeb2088ac52c
veginho <raulvegakinder@gmail.com>
2022-11-06 16:02:06 +0200

    Implementare constructor coada

---
+++ queue.java
@@ -0,0 +1,28 @@
+class Queue
+{
+    private int[] arr;      // array to store queue elements
+    private int front;      // front points to the front element in the queue
+    private int rear;       // rear points to the last element in the queue
+    private int capacity;   // maximum capacity of the queue
+    private int count;      // current size of the queue
+ 
+    // Constructor to initialize a queue
+    Queue(int size)
+    {
+        arr = new int[size];
+        capacity = size;
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+}
+class Main
+{
+    public static void main (String[] args)
+    {
+        // create a queue of capacity 5
+        Queue q = new Queue(5);
+ 
+        
+    }
+}
